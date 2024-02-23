a451a18f116fbd1d0db2a1c2c336928eb8369b2f
thecknt <barretocristianr@gmail.com>
2024-02-23 17:31:33 -0300

    added class sales

--- src/main/java/hackaton/Viajes/controller/MainController.java
+++ src/main/java/hackaton/Viajes/controller/MainController.java
@@ -1,4 +1,4 @@
-package hackaton.Viajes;
+package hackaton.Viajes.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/hackaton/Viajes/model/Client.java
+++ src/main/java/hackaton/Viajes/model/Client.java
@@ -17,7 +17,7 @@ import lombok.ToString;
 public class Client extends Person{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Integer idClient;
 
 }
