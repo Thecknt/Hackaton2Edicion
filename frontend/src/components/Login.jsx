e59e3de88f70dc28f15a810555eb8192b7187d31
sebastianjmacharette <dev@macharette.com.ar>
2024-03-10 11:01:13 -0300

    medios pagos reveer

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,26 +1,60 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate desde react-router-dom
 import axios from 'axios';
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+    const [token, setToken] = useState('');
+    const navigate = useNavigate(); // Utiliza useNavigate para la navegación
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const loginUrl = 'http://localhost:8080/login';
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
         try {
-            const response = await axios.post('/login', { username, password });
-            const { token } = response.data;
 
-            // Guardar el token en el almacenamiento local
-            localStorage.setItem('token', token);
+            const response = await axios.post(loginUrl, { username, password }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-            // Redirigir al usuario al index u otra página después de iniciar sesión
-            window.location.href = '/index';
+            if (response.data) {
+                alert('Inicio de sesión exitoso');
+
+                //Aca obtengo el token de la cabecera, lo guardo en la variable y lo pusheo al localStorage
+                const token = response.headers['x-auth-token'];
+                localStorage.setItem('token', token);
+
+                navigate('/about');
+            } else {
+                alert('Error en el inicio de sesión');
+            }
         } catch (error) {
-            console.error('Error de inicio de sesión:', error);
-            setError('Usuario o contraseña incorrectos');
+            if (error.response) {
+                console.error('Error en el inicio de sesión:', error.response.data);
+                alert(error.response ? error.response.data : 'Error en el inicio de sesión');
+
+            } else if (error.request) {
+                console.error('Error en el inicio de sesión:', error.request);
+                alert('Error en el inicio de sesión: Request failed');
+            } else {
+                console.error('Error en el inicio de sesión:', error.message);
+                alert('Error en el inicio de sesión: ' + error.message);
+            }
         }
+    };
+
+    //Esta funcion sebas es para saber si el usuario esta authenticado
+    const isAuthenticated = () => {
+        return localStorage.getItem('token') != null;
+    };
+
+    //Esta funcion es para desloguearse y sacar el token del localStorage
+    const logout = () => {
+        localStorage.removeItem('token');
     };
 
     return (
